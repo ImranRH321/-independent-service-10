646de5ca9de790fbf96e276021b56d7e8ec87122
ImranRH321 <imranrh247@gmail.com>
2022-09-17 01:33:58 +0600

    change-text

--- src/componet/Home/About/About.js
+++ src/componet/Home/About/About.js
@@ -2,13 +2,10 @@ import React from "react";
 
 const About = () => {
   const myself =
-    "https://i.ibb.co/frhDHz6/IMG-164308271536-F-removebg-preview-1-modified.png";
+    "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80";
 
   return (
     <div className="my-4">
-      <h3 className="bg-dark text-white ms-3 p-2 w-50 LTitle my-4">
-        About us for me <mark className="text-success LTitle ">Imran Rx</mark>
-      </h3>
       <div className="row container">
         <div className="col-md-6 col-sm-12">
           <div>
@@ -16,15 +13,7 @@ const About = () => {
           </div>
         </div>
         <div className="col-md-6 col-sm-12">
-          <p>
-           MY Target is Goal Facebook , Google Job 
-           Waiting .....
-          </p>
-          <p>
-            <small>
-              Ei bisoy tar opar kico feedbreak diyen vai..
-            </small>
-          </p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi quis, nihil voluptatem expedita, voluptatum, sed magni consequuntur perferendis quod ab recusandae consequatur. Voluptatibus, soluta. Modi eligendi, nam repudiandae libero veritatis nisi accusamus nulla veniam exercitationem possimus laboriosam dignissimos culpa tempore maiores voluptas harum. Expedita aperiam dolorem in amet repudiandae unde!
         </div>
       </div>
     </div>
